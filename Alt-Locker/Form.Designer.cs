acb524dc1731358aed1fc1bc4c5c5960d7b812e7
HY.Joo <jooc12@naver.com>
2025-08-13 15:36:17 +0900

    first commit

---
+++ Alt-Locker/Form.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace AltLocker
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Block the Alt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.alt차단하기_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(215, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Unblock the Alt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.alt차단풀기_MouseClick);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 107);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form";
+            this.Text = "Alt Locker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Alt-Locker/Form.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace AltLocker
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private static readonly byte[] BinaryData =
+        {
+            // 헤더(8바이트): 00 00 00 00 00 00 00 00
+            0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,
+
+            // 매핑 개수(DWORD, 종료 포함): 3
+            0x03,0x00,0x00,0x00,
+
+            // 매핑1: 0x38(Left Alt) -> 0x00 (Disable)
+            // 형식: TO(00 00) FROM(38 00)  => 00 00 38 00
+            0x00,0x00,0x38,0x00,
+
+            // 매핑2: 0xE038(Right Alt/AltGr) -> 0x00 (Disable)
+            // 형식: TO(00 00) FROM(38 E0)  => 00 00 38 E0
+            0x00,0x00,0x38,0xE0,
+
+            // 종료 엔트리
+            0x00,0x00,0x00,0x00
+        };
+
+        public Form()
+        {
+            InitializeComponent();
+            this.Load += Form_Load;
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            if (IsAltBlocked())
+            {
+                // Block 상태 → Button1 강조
+                button1.BackColor = SystemColors.Highlight;
+                button1.ForeColor = Color.White;
+                button2.BackColor = SystemColors.Control;
+                button2.ForeColor = Color.Black;
+            }
+            else
+            {
+                // Unblock 상태 → Button2 강조
+                button2.BackColor = SystemColors.Highlight;
+                button2.ForeColor = Color.White;
+                button1.BackColor = SystemColors.Control;
+                button1.ForeColor = Color.Black;
+            }
+        }
+
+        private bool IsAltBlocked()
+        {
+            using (var regKey =
+                Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Keyboard Layout", writable: false))
+            {
+                if (regKey == null) return false;
+
+                var value = regKey.GetValue("Scancode Map") as byte[];
+                if (value == null) return false;
+
+                // BinaryData와 동일하면 블록 상태
+                return value.SequenceEqual(BinaryData);
+            }
+        }
+
+        private void alt차단풀기_MouseClick(object sender, EventArgs e)
+        {
+            {
+                using (var regKey =
+                   Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Keyboard Layout", writable: true))
+                {
+                    regKey?.DeleteValue("Scancode Map", false);
+                }
+
+                MessageBox.Show("해제 완료. 재부팅(또는 로그오프) 후 반영됩니다.\nDisabled successfully. Changes will take effect after a reboot (or log off).");
+                Form_Load(null, null); // 버튼 색 갱신
+            }
+        }
+
+        private void alt차단하기_MouseClick(object sender, EventArgs e)
+        {
+            using (var regKey =
+                   Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Keyboard Layout", writable: true)
+                   ?? Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Keyboard Layout"))
+            {
+                regKey.SetValue("Scancode Map", BinaryData, RegistryValueKind.Binary);
+            }
+
+            MessageBox.Show("적용 완료. 재부팅(또는 로그오프) 후 반영됩니다.\nApplied successfully. Changes will take effect after a reboot (or log off).");
+            Form_Load(null, null); // 버튼 색 갱신
+        }
+    }
+}
